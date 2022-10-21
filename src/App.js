2eb253101f766e5cccc7572d097f3249c948f237
Mohd-Haseeb <mohd.haseeb440@gmail.com>
2022-10-21 11:46:37 +0530

    React - Learnt props and useStatehooks

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Card from './components/Cards';
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter/>
+        <Card name="Mohd Haseeb"/>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const Counter = () => {
+    let [count, setCount] = useState(0)
+    return (
+        <>
+            <h1>Counter Value - { count } </h1>
+            <button onClick={() => setCount(0)}>Reset Counter</button>
+            <button onClick={() => setCount(count+1)}>Increase Counter</button>
+            <button onClick={() => {setCount(count===0 ? count: count-1)}}>Decrease Counter</button>
+        </>
+    )
+}
+
+export default Counter
